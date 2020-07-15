0f6dcff1259e49c30feea23a273531c2c26a876f
Michael Burrows <michaelburrows@outlook.com>
2020-07-15 13:33:38 +1000

    Assigned weather and location variables.

--- ES6 Modules: How to use import and export in JavaScript/app.js
+++ ES6 Modules: How to use import and export in JavaScript/app.js
@@ -1,8 +1,7 @@
 import { country, state, city } from "./src/location.js";
 import { weather } from "./src/weather.js";
 
-const currentWeather = weather();
-
-document.getElementById("app").innerHTML = 
-"<h1>" + country +  " | " + state + " | " + city +  "</h1>" + 
-"<h2>Weather " + currentWeather + "</h2>";
+const getWeather = weather();
+const currentWeather = "<h2>Weather " + getWeather + "</h2>";
+const currentLocation = "<h1>" + country +  " | " + state + " | " + city +  "</h1>";
+document.getElementById("app").innerHTML = currentLocation + currentWeather;
