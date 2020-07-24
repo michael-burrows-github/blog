633aca425d8cd1d667b88da13a80c898f2deaa71
Michael Burrows <michaelburrows@outlook.com>
2020-07-24 18:06:55 +1000

    Initial commit.

---
+++ Preview selected image in input type=”file” using JavaScript/script.js
@@ -0,0 +1,18 @@
+const chooseFile = document.getElementById("choose-file");
+const imgPreview = document.getElementById("img-preview");
+
+chooseFile.addEventListener("change", function () {
+  getImgData();
+});
+
+function getImgData() {
+  const files = chooseFile.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = '<img src="' + this.result + '" />';
+    });    
+  }
+}
