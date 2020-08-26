5e1d17145e5901ce6c9c44ce769c2f6a04c5453b
Michael Burrows <michaelburrows@outlook.com>
2020-08-26 17:29:11 +1000

    Initial commit.

---
+++ 2020/031 - How to create a scroll to top button with JavaScript/script.js
@@ -0,0 +1,25 @@
+const scrollTop = function () {
+  // create HTML button element
+  const scrollBtn = document.createElement("button");
+  scrollBtn.innerHTML = "&uarr;";
+  scrollBtn.setAttribute("id", "scroll-btn");
+  document.body.appendChild(scrollBtn);
+  // hide/show button based on scroll distance
+  const scrollBtnDisplay = function () {
+    window.scrollY > window.innerHeight
+      ? scrollBtn.classList.add("show")
+      : scrollBtn.classList.remove("show");
+  };
+  window.addEventListener("scroll", scrollBtnDisplay);
+  // scroll to top when button clicked
+  const scrollWindow = function () {
+    if (window.scrollY != 0) {
+      setTimeout(function () {
+        window.scrollTo(0, window.scrollY - 50);
+        scrollWindow();
+      }, 10);
+    }
+  };
+  scrollBtn.addEventListener("click", scrollWindow);
+};
+scrollTop();
