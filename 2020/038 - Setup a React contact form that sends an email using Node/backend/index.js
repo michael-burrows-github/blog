6427bb8ed9928d3ad049670f1363bd6adf12a066
Michael Burrows <michaelburrows@outlook.com>
2020-09-23 12:18:59 +1000

    Initial commit.

---
+++ 2020/038 - Setup a React contact form that sends an email using Node/backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+  host: "smtp.example.com",
+  port: 587,
+  auth: {
+    user: "username@example.com",
+    pass: "password",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message; 
+  const mail = {
+    from: name,
+    to: "username@example.com",
+    subject: "Contact Form Message",
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "failed" });
+    } else {
+      res.json({ status: "sent" });
+    }
+  });
+});
