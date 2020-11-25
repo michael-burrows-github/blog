0b6912f6adfcfa46454ee8ff060831c8e0e4337e
Michael Burrows <michaelburrows@outlook.com>
2020-11-26 09:27:11 +1100

    Initial commit.

---
+++ 2020/048 - Style a responsive navbar component with Tailwind CSS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger").onclick = function toggleMenu() {
+  const navToggle = document.getElementsByClassName("toggle");
+  for (let i = 0; i < navToggle.length; i++) {
+    navToggle.item(i).classList.toggle("hidden");
+  }
+};
