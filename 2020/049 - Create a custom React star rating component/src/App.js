86e937e6726796ac8b96f986e25a354bb219f685
Michael Burrows <michaelburrows@outlook.com>
2020-12-04 13:45:31 +1100

    Initial commit.

---
+++ 2020/049 - Create a custom React star rating component/src/App.js
@@ -0,0 +1,12 @@
+import StarRating from "./StarRating";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <StarRating />
+    </div>
+  );
+};
+
+export default App;
---
+++ 2020/049 - Create a custom React star rating component/src/StarRating.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "on" : "off"}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
